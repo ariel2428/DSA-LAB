e19b2afddfc1e53773ab2bf83888ec2d0633879f
ariel2428 <ariellambo22@gmail.com>
2026-04-28 14:12:40 +0800

    Update Rodriguez.py

--- Rodriguez.py
+++ Rodriguez.py
@@ -3,67 +3,80 @@ class Node:
         self.data = data
         self.next = None
 
+def insertNodeAtTheBeginning(data):
+    global head
+    newNode = Node(data)
 
-node1 = Node("Bakit ba ikaw ang siyang iniisip ko")
+    if (head == None):
+        head = newNode
+    else:
+        newNode.next = head
+        head = newNode
+
+
+def insertNodeAtTheEnd(data):
+    global head
+    newNode = Node(data)
+
+    if (head == None):
+        head = newNode
+    else:
+        current = head
+        while (current.next != None):
+            current = current.next
+        current.next = newNode
+
+
+def insertNodeAfterGivenNode(data, node):
+    global head
+    newNode = Node(data)
+
+    if (head == None):
+        head = newNode
+    else:
+        current = head
+        prev = None
+
+        while (prev == None or prev.data != node):
+            prev = current
+            current = current.next
+
+            if (current == None):
+                print('The node does not exist')
+                return
+
+        newNode.next = current
+        prev.next = newNode
+
+
+def traverseLinkedList():
+    current = head
+    while (current):
+        print(current.data, end=" -> ")
+        current = current.next
+    print("None")
+
+
+#Main Program
+node1 = Node('Buksan mo by Willie Revillame')
 print(node1.data)
-
+print()
 
 head = node1
 
+insertNodeAtTheBeginning('Ale by The Bloomfields')
+insertNodeAtTheBeginning('Mundo by IV of Spades')
+insertNodeAtTheBeginning('Dati by Sam Concepcion')
 
-def insertNodeAtTheBeginning(head, data):
-    new_node = Node(data)
-    new_node.next = head
-    head = new_node
-    return head
+traverseLinkedList()
+print()
 
+insertNodeAtTheEnd('Kahit Ayaw Mo Na by This Band')
+insertNodeAtTheEnd('Paraluman by Adie')
 
-head = insertNodeAtTheBeginning(head, "Ako'y alipin mo kahit hindi batid")
-head = insertNodeAtTheBeginning(head, "Araw-gabi nasa isip ka")
+traverseLinkedList()
+print()
 
-
-def traverseLinkedList(head):
-    current = head
-    while current is not None:
-        print(current.data)
-        current = current.next
-
-
-def insertNodeAtTheEnd(head, data):
-    new_node = Node(data)
-    
-    if head is None:
-        return new_node
-    
-    temp = head
-    while temp.next is not None:
-        temp = temp.next
-    
-    temp.next = new_node
-    return head
-
-
-head = insertNodeAtTheEnd(head, "Di ko mapigilan ang damdamin ko")
-head = insertNodeAtTheEnd(head, "Sana'y mapansin mo ang puso ko")
-
-print("\nAfter inserting at the end:")
-traverseLinkedList(head)
-
-
-def insertNodeAtGivenNode(head, prev_value, data):
-    temp = head
-    
-    while temp is not None:
-        if temp.data == prev_value:
-            new_node = Node(data)
-            new_node.next = temp.next
-            temp.next = new_node
-            return head
-        temp = temp.next
-    
-    print("Previous node not found")
-    return head
-
-
-head = insertNodeAtGivenNode(head, "Ako'y alipin mo kahit hindi batid", "Pag-ibig ko'y tunay at wagas")
-head = insertNodeAtGivenNode(head, "Araw-gabi nasa isip ka", "Ikaw lamang ang laman ng puso")
+insertNodeAfterGivenNode('Ikaw at Ako by Moira', 'Ale by The Bloomfields')
+insertNodeAfterGivenNode('Ere by Eraserheads', 'Mundo by IV of Spades')
+traverseLinkedList()
